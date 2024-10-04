2c71f8a1a6161dbc0f15101ded984bfe90090d54
Girishmasade <girishmasade@gmail.com>
2024-10-04 22:57:34 +0530

    Homepage created successfully

--- src/pages/Head.jsx
+++ src/pages/Head.jsx
@@ -1,18 +1,66 @@
-import React from "react";
-import { head } from "../utils/HeadData";
+import React from 'react';
+import {Table, Tag } from 'antd';
+import { head } from '../utils/HeadData';
+const columns = [
+  {
+    title: 'Business name',
+    width: 150,
+    dataIndex: 'name',
+    key: 'name',
+    fixed: 'left',
+  },
+  {
+    title: 'Business PAN',
+    width: 100,
+    dataIndex: 'Pan',
+    key: 'Pan',
+    fixed: 'left',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'Email',
+    key: 'Email',
+    width: 150,
+  },
+  {
+    title: 'Connection Type',
+    dataIndex: 'connection',
+    key: 'connection',
+    width: 150,
+   
+  },
+  {
+    title: 'Authorised By',
+    dataIndex: 'sent',
+    key: 'sent',
+    width: 150,
+  },
 
-function Head() {
+  {
+    title: 'Action',
+    key: 'operation',
+    fixed: 'right',
+    width: 100,
+    render: () => <a className='text-blue-500'>view</a>,
+  },
+];
+const data = head.map(item => ({
+  key: item.id,
+  name: item.name,
+  age: item.age,
+  Pan: item.Pan,
+  Email: item.Email,
+  connection: item.connection,
+  sent: item.sent
+}));
+
+const Head = () => {
   return (
-    <div className="flex ">
-        {head.map((idx) => (
-      <table key={idx.id} className="w-full border">
-          <tr>
-            <th className="p-3 bg-gray-100">{idx.th}</th>
-          </tr>
-      </table>
-        ))}
-    </div>
+    <Table
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+    />
   );
-}
-
+};
 export default Head;
